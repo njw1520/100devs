4faa882c77d01b6d0c70ebb6737b54f96b2fe69d
Nate Webb <145239754+njw1520@users.noreply.github.com>
2024-04-22 21:56:01 -0400

    Completed in class assignments

--- class-14/class14-materials/simple-functions/js/main.js
+++ class-14/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,17 @@
 //create a function that adds two numbers and alerts the sum
 
+function addTwo(a, b) {
+  alert(a + b);
+}
+
 //create a function that multiplys three numbers and console logs the product
 
+function multThree(a, b, c) {
+  console.log(a * b * c);
+}
+
 //create a function that divides two numbers and returns the ???
+
+function divideTwo(a, b) {
+  return a / b;
+}
--- class-14/class14-materials/temperature-converter/js/main.js
+++ class-14/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,16 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+// Get temp (C)
+//
+document.querySelector("#temp").addEventListener("click", run);
+
+function run() {
+  let temp = document.querySelector("#temp").value;
+
+  // Convert Temp to C
+
+  temp = (temp * 9) / 5 + 32;
+
+  // Display converted Temp]
+
+  document.querySelector("f_temp").innerText = temp;
+}
